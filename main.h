8c6e0613dfc33b90940a267b598c2c7ac908e997
Aydan Jeferlee <146703804+lunarinaa@users.noreply.github.com>
2023-12-30 20:28:48 +0400

    header file updated

--- main.h
+++ main.h
@@ -7,11 +7,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+
+/* globar variable */
 extern char **environ;
+
+/* function prototypes */
 char **splitter(char *str, char *delim);
 char *_getline(void);
 char **pathfinder(void);
 void executer(char *command, char **array);
 void environ_printer(void);
 void array_cleaner(char **array);
+
 #endif
